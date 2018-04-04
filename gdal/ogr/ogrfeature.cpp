599e364bc95ab9e4cb280bd49daf5e64bb1db9fa
Even Rouault <even.rouault@spatialys.com>
2018-04-04 20:42:27 +0200

    More uses of ::FromHandle / ::ToHandle

--- gdal/ogr/ogrfeature.cpp
+++ gdal/ogr/ogrfeature.cpp
@@ -1070,7 +1070,7 @@ OGRFieldDefnH OGR_F_GetFieldDefnRef( OGRFeatureH hFeat, int i )
         return nullptr;
     }
 
-    return reinterpret_cast<OGRFieldDefnH>(poFeat->GetFieldDefnRef(i));
+    return OGRFieldDefn::ToHandle(poFeat->GetFieldDefnRef(i));
 }
 
 /************************************************************************/
@@ -1202,7 +1202,7 @@ OGRGeomFieldDefnH OGR_F_GetGeomFieldDefnRef( OGRFeatureH hFeat, int i )
 {
     VALIDATE_POINTER1( hFeat, "OGR_F_GetGeomFieldDefnRef", nullptr );
 
-    return reinterpret_cast<OGRGeomFieldDefnH>(
+    return OGRGeomFieldDefn::ToHandle(
         OGRFeature::FromHandle(hFeat)->GetGeomFieldDefnRef(i));
 }
 
