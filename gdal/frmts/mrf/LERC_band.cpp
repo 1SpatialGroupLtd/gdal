38a1423c232566bad5f2e07b3ab593ba2094d7b6
Lucian Plesea <lucianpls@gmail.com>
2018-06-03 13:09:31 -0700

    Fix detection of Lerc2 data
    An empty Lerc2 tile can be smaller than a Lerc1 tile.  In that case, the old code failed to detect the encoding.

--- gdal/frmts/mrf/LERC_band.cpp
+++ gdal/frmts/mrf/LERC_band.cpp
@@ -441,7 +441,9 @@ CPLXMLNode *LERC_Band::GetMRFConfig(GDALOpenInfo *poOpenInfo)
     if (poOpenInfo->eAccess != GA_ReadOnly
         || poOpenInfo->pszFilename == nullptr
         || poOpenInfo->pabyHeader == nullptr
-        || strlen(poOpenInfo->pszFilename) < 2)
+        || strlen(poOpenInfo->pszFilename) < 2
+        // Header of Lerc2 takes 58 bytes, an emtpy area 62.  Lerc 1 empty file is 67.
+        || poOpenInfo->nHeaderBytes < static_cast<int>(Lerc2::ComputeNumBytesHeader()))
         return nullptr;
 
     // Check the header too
