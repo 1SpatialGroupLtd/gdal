322f6d11097402b543687d8f1a826e7c8f105700
Even Rouault <even.rouault@spatialys.com>
2018-07-13 21:39:21 +0200

    unbreak f5a02b1fb15ac0439ce13899007509b4245853b2

--- gdal/gcore/gdaldataset.cpp
+++ gdal/gcore/gdaldataset.cpp
@@ -39,8 +39,8 @@
 #include <algorithm>
 #include <map>
 #include <new>
+#include <set>
 #include <string>
-#include <unordered_set>
 #include <utility>
 
 #include "cpl_conv.h"
@@ -2697,7 +2697,7 @@ GDALDatasetH CPL_STDCALL GDALOpenEx( const char *pszFilename,
     // Prevent infinite recursion.
     struct AntiRecursionStruct
     {
-        std::unordered_set<std::string> aosDatasetNames{};
+        std::set<std::pair<std::string, int>> aosDatasetNamesWithFlags{};
         int nRecLevel = 0;
     };
     static thread_local AntiRecursionStruct sAntiRecursion;
@@ -2707,12 +2707,12 @@ GDALDatasetH CPL_STDCALL GDALOpenEx( const char *pszFilename,
                     "GDALOpen() called with too many recursion levels");
         return nullptr;
     }
-    if( sAntiRecursion.aosDatasetNames.find(
-            std::string(pszFilename)) !=
-                sAntiRecursion.aosDatasetNames.end() )
+    if( sAntiRecursion.aosDatasetNamesWithFlags.find(
+            std::pair<std::string, int>(std::string(pszFilename), nOpenFlags)) !=
+                sAntiRecursion.aosDatasetNamesWithFlags.end() )
     {
         CPLError(CE_Failure, CPLE_AppDefined,
-                    "GDALOpen() called with recursively");
+                    "GDALOpen() called on %s recursively", pszFilename);
         return nullptr;
     }
 
@@ -2795,7 +2795,8 @@ GDALDatasetH CPL_STDCALL GDALOpenEx( const char *pszFilename,
 #endif
 
         sAntiRecursion.nRecLevel ++;
-        sAntiRecursion.aosDatasetNames.insert(std::string(pszFilename));
+        sAntiRecursion.aosDatasetNamesWithFlags.insert(
+            std::pair<std::string, int>(std::string(pszFilename), nOpenFlags));
 
         GDALDataset *poDS = nullptr;
         if ( poDriver->pfnOpen != nullptr )
@@ -2812,7 +2813,8 @@ GDALDatasetH CPL_STDCALL GDALOpenEx( const char *pszFilename,
         }
 
         sAntiRecursion.nRecLevel --;
-        sAntiRecursion.aosDatasetNames.erase(std::string(pszFilename));
+        sAntiRecursion.aosDatasetNamesWithFlags.erase(
+            std::pair<std::string, int>(std::string(pszFilename), nOpenFlags));
 
         CSLDestroy(papszTmpOpenOptions);
         CSLDestroy(papszTmpOpenOptionsToValidate);
