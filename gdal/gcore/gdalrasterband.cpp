96f70ea2cc6fd965760971c2941853bb8284c1e9
Even Rouault <even.rouault@spatialys.com>
2019-08-11 02:07:48 +0200

    multidim: add 'final' keywords and a few fixes for issues raised by cppcheck

--- gdal/gcore/gdalrasterband.cpp
+++ gdal/gcore/gdalrasterband.cpp
@@ -7070,7 +7070,7 @@ void GDALRasterBand::InitRWLock()
 /*                     GDALMDArrayFromRasterBand                        */
 /************************************************************************/
 
-class GDALMDArrayFromRasterBand: public GDALMDArray
+class GDALMDArrayFromRasterBand final: public GDALMDArray
 {
     CPL_DISALLOW_COPY_ASSIGN(GDALMDArrayFromRasterBand)
 
