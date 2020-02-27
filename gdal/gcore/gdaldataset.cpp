a0b5af2261d031e8cd281a1279c50b31de8f069e
Even Rouault <even.rouault@spatialys.com>
2020-02-27 23:24:20 +0100

    Fix various cppcheck warnings about test always evaluating to true/false

--- gdal/gcore/gdaldataset.cpp
+++ gdal/gcore/gdaldataset.cpp
@@ -864,7 +864,7 @@ const char *GDALDataset::GetProjectionRefFromSpatialRef(const OGRSpatialReferenc
     {
         return "";
     }
-    if( pszWKT && m_poPrivate->m_pszWKTCached &&
+    if( m_poPrivate->m_pszWKTCached &&
         strcmp(pszWKT, m_poPrivate->m_pszWKTCached) == 0 )
     {
         CPLFree(pszWKT);
@@ -1552,7 +1552,7 @@ const char *GDALDataset::GetGCPProjectionFromSpatialRef(const OGRSpatialReferenc
     {
         return "";
     }
-    if( pszWKT && m_poPrivate->m_pszWKTGCPCached &&
+    if( m_poPrivate->m_pszWKTGCPCached &&
         strcmp(pszWKT, m_poPrivate->m_pszWKTGCPCached) == 0 )
     {
         CPLFree(pszWKT);
