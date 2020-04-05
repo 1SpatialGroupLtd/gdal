66252bd9037d2ef8a5588eea75ec89d6fb93baf3
Even Rouault <even.rouault@spatialys.com>
2020-04-05 18:40:16 +0200

    GDALDataset::SetProjection(): re-allow the use of PROJ4 strings (fixes #2034)

--- gdal/gcore/gdaldataset.cpp
+++ gdal/gcore/gdaldataset.cpp
@@ -1003,7 +1003,7 @@ CPLErr GDALDataset::SetProjection( const char *pszProjection )
     {
         OGRSpatialReference oSRS;
         oSRS.SetAxisMappingStrategy(OAMS_TRADITIONAL_GIS_ORDER);
-        if( oSRS.importFromWkt(pszProjection) != OGRERR_NONE )
+        if( oSRS.SetFromUserInput(pszProjection) != OGRERR_NONE )
         {
             return CE_Failure;
         }
