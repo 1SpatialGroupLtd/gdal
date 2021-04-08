10730888f167162ef8ea3555d4e5dbd2b1410913
Even Rouault <even.rouault@spatialys.com>
2021-04-08 15:54:52 +0200

    GPKG: take into account OGR_CURRENT_DATE config option in gpkg_metadata_reference table (fixes #3537)

--- gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagetablelayer.cpp
+++ gdal/ogr/ogrsf_frmts/gpkg/ogrgeopackagetablelayer.cpp
@@ -1510,6 +1510,27 @@ OGRErr OGRGeoPackageTableLayer::CreateGeomField( OGRGeomFieldDefn *poGeomFieldIn
         CPLString osSQL(pszSQL);
         sqlite3_free(pszSQL);
 
+        if( m_poDS->HasExtensionsTable() )
+        {
+            // Suppress gdal_aspatial extension if this was the last
+            // aspatial layer.
+            bool bHasASpatialLayers = false;
+            for(int i=0;i<m_poDS->GetLayerCount();i++)
+            {
+                if( m_poDS->GetLayer(i) != this &&
+                    m_poDS->GetLayer(i)->GetLayerDefn()->GetGeomFieldCount() == 0 )
+                    bHasASpatialLayers = true;
+            }
+            if( !bHasASpatialLayers )
+            {
+                osSQL +=
+                    ";"
+                    "DELETE FROM gpkg_extensions WHERE "
+                    "extension_name = 'gdal_aspatial' "
+                    "AND table_name IS NULL "
+                    "AND column_name IS NULL";
+            }
+        }
         OGRErr err = SQLCommand(m_poDS->GetDB(), osSQL);
         if ( err != OGRERR_NONE )
             return err;
@@ -4096,11 +4117,14 @@ OGRErr OGRGeoPackageTableLayer::RunDeferredCreationIfNecessary()
     const bool bIsSpatial = (eGType != wkbNone);
     if ( bIsSpatial )
         err = RegisterGeometryColumn();
+    else if( m_eASpatialVariant == OGR_ASPATIAL )
+        err = m_poDS->CreateGDALAspatialExtension();
 
     if ( err != OGRERR_NONE )
         return OGRERR_FAILURE;
 
     if( bIsSpatial ||
+        m_eASpatialVariant == OGR_ASPATIAL ||
         m_eASpatialVariant == GPKG_ATTRIBUTES )
     {
         const char* pszIdentifier = GetMetadataItem("IDENTIFIER");
@@ -4109,18 +4133,16 @@ OGRErr OGRGeoPackageTableLayer::RunDeferredCreationIfNecessary()
         const char* pszDescription = GetMetadataItem("DESCRIPTION");
         if( pszDescription == nullptr )
             pszDescription = "";
-        const char* pszCurrentDate = CPLGetConfigOption("OGR_CURRENT_DATE", nullptr);
-        CPLString osInsertGpkgContentsFormatting("INSERT INTO gpkg_contents "
-                 "(table_name,data_type,identifier,description,last_change,srs_id) VALUES "
-                "('%q','%q','%q','%q',");
-        osInsertGpkgContentsFormatting += ( pszCurrentDate ) ? "'%q'" : "%s";
-        osInsertGpkgContentsFormatting += ",%d)";
 
         pszSQL = sqlite3_mprintf(
-            osInsertGpkgContentsFormatting.c_str(),
-            pszLayerName, (bIsSpatial ? "features": "attributes" ),
+            "INSERT INTO gpkg_contents "
+            "(table_name,data_type,identifier,description,last_change,srs_id) VALUES "
+            "('%q','%q','%q','%q',%s,%d)",
+            pszLayerName, (bIsSpatial ? "features":
+                          (m_eASpatialVariant == GPKG_ATTRIBUTES) ? "attributes" :
+                          "aspatial"),
             pszIdentifier, pszDescription,
-            pszCurrentDate ? pszCurrentDate : "strftime('%Y-%m-%dT%H:%M:%fZ','now')",
+            GDALGeoPackageDataset::GetCurrentDateEscapedSQL().c_str(),
             m_iSrs);
 
         err = SQLCommand(m_poDS->GetDB(), pszSQL);
